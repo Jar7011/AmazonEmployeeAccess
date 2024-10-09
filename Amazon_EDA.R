cf33a9e60bf9a98b8b4cd302bc5dcbe7f58b0909
Jar7011 <jareyes1324@gmail.com>
2024-10-08 23:07:12 -0600

    Created exploratory plots and transformed data with dummy variables

--- Amazon_EDA.R
+++ Amazon_EDA.R
@@ -1,8 +1,28 @@
 library(vroom)
 library(embed)
 library(tidymodels)
+library(DataExplorer)
+
 
 # Read in the data
 train_data <- vroom('train.csv')
 test_data <- vroom('test.csv')
 
+# Simple Exploratory Plots
+plot_intro(train_data)
+plot_missing(train_data)
+plot_correlation(train_data)
+
+
+# Create a recipe
+my_recipe <- recipe(ACTION ~ ., data = train_data) %>% 
+  step_mutate_at(all_predictors(), fn = factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .001, other = 'Other') %>% 
+  step_dummy(all_nominal_predictors())
+
+# Prep and bake recipe
+prep <- prep(my_recipe)  
+baked <- bake(prep, new_data = train_data)
+
+# Get number of columns
+ncol(baked)

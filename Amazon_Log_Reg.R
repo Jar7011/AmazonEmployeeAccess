75b608d61f81bc1fdd3b63cadc0f586d22995bbb
Jar7011 <jareyes1324@gmail.com>
2024-10-10 23:39:13 -0600

    Finished logistic regression model and predictions

---
+++ Amazon_Log_Reg.R
@@ -0,0 +1,41 @@
+library(vroom)
+library(tidymodels)
+library(tidyverse)
+
+## Logistic Regression ##
+
+# Read in the data
+train_data <- vroom('train.csv')
+test_data <- vroom('test.csv')
+
+# Change ACTION to factors
+train_data <- train_data %>% 
+  mutate(ACTION = as.factor(ACTION))
+
+# Create a recipe
+log_reg_recipe <- recipe(ACTION ~ ., data = train_data) %>% 
+  step_mutate_at(all_predictors(), fn = factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .001, other = 'Other') %>% 
+  step_dummy(all_nominal_predictors())
+
+# Create logistic regression model
+log_reg_model <- logistic_reg() %>% 
+  set_engine('glm')
+
+# Create workflow
+log_reg_wf <- workflow() %>% 
+  add_recipe(log_reg_recipe) %>% 
+  add_model(log_reg_model) %>% 
+  fit(data = train_data)
+
+# Predict and format for submission
+log_reg_preds <- predict(log_reg_wf, 
+                         new_data = test_data,
+                         type = "prob") %>% 
+  mutate(id = row_number(), ACTION = .pred_1) %>% 
+  select(id, ACTION)
+
+# Write out the file
+vroom_write(x = log_reg_preds, file = "./Logistic_Reg.csv", delim = ",")
+
+# Score: 0.80913
